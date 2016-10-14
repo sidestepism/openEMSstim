0a7668b7e1e34a7bc2a5ae4849e4ef2a7a3e0c44
sidestepism <fushimi.ryohe.i@gmail.com>
2016-10-15 03:36:17 +0900

    import from sidestepism/Vibrat-o-matic

---
+++ vibratomatic/index.js
@@ -0,0 +1,126 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var SerialPort = require('serialport');
+
+var defaultSerialPort = "/dev/tty.usbserial-A9O3R1XL"
+var portName = process.argv.length > 2 ? process.argv[3] : defaultSerialPort;
+var updateInterval = 30 // [msec]
+
+console.log("connecting to ", portName)
+
+// ensure f(0) = f(1) = 0
+var envelopeFunction = {
+	square: function(x) { return 1 },
+	sin: function(x) { return Math.sin(x*Math.PI) },
+	sin3: function(x) { return Math.pow(Math.sin(x*Math.PI), 3) },
+	sinr3: function(x) { return Math.pow(Math.sin(x*Math.PI), 1/3) },
+	sina: function(x) { return Math.sin(x*Math.PI)*1/2 + 1/2 },
+}
+
+app.use(express.static('static'));
+
+app.get('/', function(req, res){
+  res.sendFile('static/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	var updateIntervalTimer;
+
+	socket.on('test', function (data) {
+	var startDate = +(new Date())
+	if (data.updateInterval) updateInterval = data.updateInterval
+
+	if (data.envelopeFunction == 'default') {
+		send_command(3, data.strength, data.length)
+	}else{
+		if (!envelopeFunction[data.envelopeFunction]){
+			console.log("no such envelope function:", data.envelopeFunction)
+			return
+		}
+		if (updateIntervalTimer){
+			clearInterval(updateIntervalTimer)
+		}
+		setInterval(function (arguments) {
+			var currentDate = +(new Date())
+			var elapsedRatio = (currentDate - startDate) / data.length
+			if (elapsedRatio < 0 || elapsedRatio > 1){
+				clearInterval(updateIntervalTimer)
+				return
+			}
+			var intensity = Math.round(envelopeFunction[data.envelopeFunction](elapsedRatio) * data.strength)
+			send_command(3, intensity, updateInterval)
+		}, updateInterval)
+	}
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+var openEMSstim = new SerialPort(portName, {
+   baudRate: 57600,
+   parser: SerialPort.parsers.readline("\n")
+ });
+
+// Register functions for handling the communication with openEMSstim
+openEMSstim.on('open', showPortOpen);
+openEMSstim.on('data', sendSerialData);
+openEMSstim.on('close', showPortClose);
+openEMSstim.on('error', showError);
+
+function showPortOpen() {
+	   // console.log('openEMSstim connected to serial port at data rate: ' + openEMSstim.options.baudRate);
+}
+ 
+function sendSerialData(data) {
+	   // console.log(data);
+}
+ 
+function showPortClose() {
+	   // console.log('openEMSstim connection to serial port closed.');
+}
+ 
+function showError(error) {
+	   // console.log('Serial port error: ' + error);
+}
+
+//First validates a command, then if valid, sends it to the openEMSstim
+function send_command(channel, intensity, duration) {
+	var command = ""
+	console.log("||||||||||||||||||||||||||||||".substring(0, Math.round(intensity / 3)))
+	if (is_numeric(channel) && is_numeric(intensity) && is_numeric(duration)) {
+		if (channel == 1 || channel == 2 || channel == 3) {
+			command = "C" + (channel -1);
+		} else {
+			console.log("ERROR: Command malformatted, will not send to openEMSstim");
+			return null;
+		}
+		if (intensity >= 0 && intensity <= 100) {
+			command += "I" + intensity; 
+		} else {
+			console.log("ERROR: Command malformatted, will not send to openEMSstim");
+			return null;
+		}
+		if (duration >= 0) {
+			command += "T" + duration + "G";
+		} else {
+			console.log("ERROR: Command malformatted, will not send to openEMSstim");
+		}
+		command += "\n"
+		// console.log("sending: " + command);
+		openEMSstim.write(command);
+	} else {
+		console.log("ERROR: Command malformatted, will not send to openEMSstim");
+		return null;
+	}
+}
+
+function is_numeric(str){
+	if (!isNaN(parseInt(str, 10))) return true;
+	else return false;
+}
+
---
+++ vibratomatic/static/main.js
@@ -0,0 +1,282 @@
+"use strict";
+var socket = io();
+var youtubePlayer
+var onYouTubeIframeAPIReady;
+var onPlayerReady;
+var onPlayerStateChange;
+var youtubeRecording = false;
+var youtubePlaying = false;
+var lastParamUpdated = 0;
+
+// output JSON to textarea
+var youtubeSettings = [];
+// iterator
+var youtubeSettingsSeekIndex = 0;
+
+$(function () {
+	 /** test interface */
+	 var testEmitTimer;
+	 $("#test-length").change(function () {
+	 	$("#test-length-label").text($("#test-length").val())
+	 });
+	 $("#test-strength").change(function () {
+	 	$("#test-strength-label").text($("#test-strength").val())
+	 })
+	 $("#test-submit").click(function () {
+	 	console.log("submit")
+	 	var emit = function () {
+			socket.emit('test', {
+				// add + for numeric value
+				length: +$("#test-length").val(),
+				strength: +$("#test-strength").val(),
+				envelopeFunction: $("#test-envelope").val(),
+				updateInterval: +$("#test-update-interval").val()
+			});
+	 	}
+
+		if($("#test-repeat").prop('checked')){
+			if(testEmitTimer) clearInterval(testEmitTimer);
+			testEmitTimer = setInterval(emit, +Math.max($("#test-interval").val(), 50))
+	 	}
+
+	 	emit();
+	 });
+
+	 $("#test-repeat").change(function () {
+	 	if($("#test-repeat").prop('checked')){
+	 		// この状態でsubmitボタンを押すと繰り返し実行される
+	 	}else{
+	 		if(testEmitTimer) clearInterval(testEmitTimer);
+	 	}
+	 })
+
+	 /** sing interface */
+	var singEmitTimer;
+	$("#sing-frequency").change(function () {
+		$("#sing-frequency-label").text($("#sing-frequency").val() / 10)
+	});
+	$("#sing-strength").change(function () {
+		$("#sing-strength-label").text($("#sing-strength").val())
+	})
+	var updateDotPosition = function() {
+		if(singEmitTimer){
+			var freqRaito = ($("#sing-frequency").val() - $("#sing-frequency").attr("min"))/($("#sing-frequency").attr("max") - $("#sing-frequency").attr("min"))
+			var strengthRaito = ($("#sing-strength").val() - $("#sing-strength").attr("min"))/($("#sing-strength").attr("max") - $("#sing-strength").attr("min"))
+		}else{
+			var freqRaito = 0
+			var strengthRaito = 0	
+		}		
+		$("#sing-pad-dot").css("bottom", strengthRaito * 200 - 8).css("left", freqRaito * 200 - 8)
+	}
+	$("#sing-pad-dot").on("dragstart", function() {
+		return false;		
+	}).css("pointer-events", "none")
+
+	var vibratoOn = function(){
+		if(singEmitTimer){
+			clearTimeout(singEmitTimer);
+			return
+		}
+		var emit = function () {
+			var interval = Math.round(1000 / ($("#sing-frequency").val() / 10))
+			socket.emit('test', {
+				// add + for numeric value
+				length: Math.round(interval),
+				strength: +$("#sing-strength").val(),
+				envelopeFunction: $("#sing-envelope").val(),
+				updateInterval: +$("#sing-update-interval").val()
+			});
+			singEmitTimer = setTimeout(emit, interval);
+		}
+		emit();
+		console.log($("#sing-envelope").val())
+		$("#sing-pad").css("background-color", "lightgray");
+
+	 	// 録画
+		if (youtubeRecording && youtubePlayer.getPlayerState() == 1){
+			console.log("record", youtubePlayer.getCurrentTime())
+			youtubeSettings.push({
+				timeStamp: Math.floor(youtubePlayer.getCurrentTime()*1000)/1000,
+				event: "vibratoOn",
+				frequency: $("#sing-frequency").val(),
+				strength: $("#sing-strength").val()
+			})
+			updateSettingJSON();
+		}
+	}
+	var vibratoOff = function(){
+		if (!singEmitTimer) return;
+		clearTimeout(singEmitTimer);
+		singEmitTimer = false;
+		$("#sing-pad").css("background-color", "white");
+
+	 	// 録画
+		if (youtubeRecording && youtubePlayer.getPlayerState() == 1){
+			youtubeSettings.push({
+				timeStamp: Math.floor(youtubePlayer.getCurrentTime()*1000)/1000,
+				event: "vibratoOff",
+				frequency: $("#sing-frequency").attr("min"),
+				strength: $("#sing-strength").attr("min")
+			})
+			updateSettingJSON();
+		}
+	}
+	
+	$("#sing-vibrato").mousedown(vibratoOn).mouseup(vibratoOff)
+	$("#sing-vibrato-toggle").click(function () {
+		if(singEmitTimer) {
+			vibratoOff();
+		}else{
+			vibratoOn();
+		}
+	})
+
+	var updateParam = function (evt) {
+		if (!singEmitTimer){
+			return
+		}
+		if (+new Date() - lastParamUpdated < 50){
+			return
+		}
+		lastParamUpdated = +new Date();
+
+	 	var freq = Math.round(10 + evt.offsetX / $(evt.currentTarget).width() * 6 * 10) / 10
+	 	var strength = 100 - Math.round(evt.offsetY / $(evt.currentTarget).height() * 100)
+	 	$("#sing-frequency").val(freq * 10)
+	 	$("#sing-frequency-label").text(freq)
+	 	$("#sing-strength").val(strength)
+	 	$("#sing-strength-label").text(strength)
+	 	updateDotPosition();
+
+	 	// 録画
+		if (youtubeRecording && youtubePlayer.getPlayerState() == 1){
+			youtubeSettings.push({
+				timeStamp: Math.floor(youtubePlayer.getCurrentTime()*1000)/1000,
+				event: "updateParam",
+				frequency: $("#sing-frequency").val(),
+				strength: $("#sing-strength").val()
+			})
+			updateSettingJSON();
+		}
+	}
+	var padMouseDownFlag = false
+	$("#sing-pad").mousedown(updateParam).mousedown(function () {
+		padMouseDownFlag = true
+	}).mouseup(function () {
+		padMouseDownFlag = false
+	}).mousemove(function (evt) {
+		if(padMouseDownFlag){
+			updateParam(evt);
+		}
+	}).mouseout(function () {
+		padMouseDownFlag = false;
+		vibratoOff();
+	}).mousedown(vibratoOn).mouseup(vibratoOff);
+
+	/*
+	 * +++++++++++++++++++++++++++++
+	 * Youtube song interface
+	 * +++++++++++++++++++++++++++++
+	 */
+    var loadSettingTimer = null;
+    onYouTubeIframeAPIReady = function() {
+    	youtubePlayer = new YT.Player('youtube-player', {
+          videoId: 'vNhhAEupU4g',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+        console.log("Youtube iframe API Loaded");
+    }
+    onPlayerReady = function() {
+    	console.log("Youtube iframe API Player Ready")
+    }
+	onPlayerStateChange = function() {
+    	console.log("Youtube iframe API State Changed")
+	};
+
+    console.log("Youtube iframe API Loading");
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var youtubePlayer = null;
+    function updateSettingJSON(){
+    	$("#youtube-settings").val(JSON.stringify(youtubeSettings))
+    }
+	$("#youtube-switch").children().each(function(e){
+		$(this).click(function() {
+			youtubePlayer.cueVideoById($(this).attr("x-youtube-id"), $(this).attr("x-youtube-from"))
+		})
+	});
+	$("#youtube-record").click(function() {
+		youtubeRecording = true;
+		youtubePlaying = false;
+		$("#youtube-record").attr("disabled", true)
+		$("#youtube-play").attr("disabled", true)
+		youtubePlayer.playVideo()
+	});
+	$("#youtube-play").click(function() {
+		youtubeSettings = JSON.parse($("#youtube-settings").val());
+		youtubeRecording = false;
+		youtubePlaying = true;
+		youtubeSettingsSeekIndex = 0;
+		function loadNextSetting(){
+			var setting;
+			if(!youtubePlaying){
+				conosle.log("loadNextSetting: youtubeplaying flag is off");
+				loadSettingTimer = setTimeout(loadNextSetting, 100);
+			}
+			if(youtubePlayer.getPlayerState() != 1){
+				console.log("loadNextSetting: youtubePlayer.getPlayerState() is not playing state");
+				loadSettingTimer = setTimeout(loadNextSetting, 100);
+			}
+			if(setting = youtubeSettings[youtubeSettingsSeekIndex]){
+				console.log("loading setting:", setting)
+				switch (setting.event) {
+					case "vibratoOn":
+						$("#sing-frequency").val(setting.frequency);
+						$("#sing-strength").val(setting.strength);
+						vibratoOn();
+						break;
+					case "vibratoOff":
+						$("#sing-frequency").val(setting.frequency);
+						$("#sing-strength").val(setting.strength);
+						vibratoOff();
+						break;
+					case "updateParam":
+						$("#sing-frequency").val(setting.frequency);
+						$("#sing-strength").val(setting.strength);
+						break;
+					default:
+						console.log("unknown event:", setting.event)
+						break;
+				}
+				updateDotPosition();
+				youtubeSettingsSeekIndex ++
+				if(setting = youtubeSettings[youtubeSettingsSeekIndex]){
+					loadSettingTimer = setTimeout(loadNextSetting, (setting.timeStamp - youtubePlayer.getCurrentTime()) * 1000);
+				}
+			}else{
+				console.log("loadNextSetting: Seekindex out of range", youtubeSettingsSeekIndex)
+			}
+		}
+
+		$("#youtube-record").attr("disabled", true)
+		$("#youtube-play").attr("disabled", true)
+		youtubePlayer.playVideo();
+		if (loadSettingTimer){
+			clearTimeout(loadSettingTimer)
+		}
+		loadSettingTimer = setTimeout(loadNextSetting, 100);
+	});
+	$("#youtube-stop").click(function() {
+		youtubeRecording = false;		
+		youtubePlaying = false;
+		$("#youtube-record").attr("disabled", false)
+		$("#youtube-play").attr("disabled", false)
+		youtubePlayer.stopVideo()
+	});
+
+})
